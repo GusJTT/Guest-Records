b2e7dd7c2ab97611bb7d6fb34162834db6b61b43
Gustavo Turpo Torres <Gustavo Turpo Torres>
2020-07-01 16:25:38 -0500

    Archivos para manejo de reservaciones, aun sin funcionamiento

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Reservacion/Reservacion.h"
+int main() {
+    int cant = 0;
+
+    int reservaciones[50];
+    while(cant < 50){
+
+    }
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
