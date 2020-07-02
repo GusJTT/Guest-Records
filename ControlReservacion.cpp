b5a65d1d07fd15da673470965350c0ae6fafbcea
Gustavo Turpo Torres <gturpot@unsa.edu.pe>
2020-07-01 21:03:26 -0500

    Funcion verReservaciones() funcional (solo funciona en memoria)

--- ControlReservacion.cpp
+++ ControlReservacion.cpp
@@ -1,52 +1,35 @@
-//
-// Created by G-t-T on 1/07/2020.
-//
 #include <iostream>
-#include "Reservacion/Reservacion.h"
-#include "TipoHabitacion/TipoHabitacion.h"
-#include "Habitacion/Habitacion.h"
-
-static void agregarReservacion(Reservacion r[50], int& cant){
-    /*
-    Habitacion habitacion;
-    int codigo;
-    int cantidadPersonas;
-    time_t fechaIngreso;
-    time_t fechaSalida;
-     */
-    TipoHabitacion tp("individual", "una persona", 50);
-    int ax1;
-    time_t ax2;
-    cout << "->Agregar Reservaciones" << endl;
-    cout << "  Ingrese el codigo de reservacion: ";
-    cin >> ax1;
-    r[cant].setCodigo(ax1);
-    cout << "  Ingrese el numero de habitacion: ";
-    cin >> ax1;
-    Habitacion hab(ax1,1,tp);
-    r[cant].setHabitacion(hab);
-    cout << "  Ingrese la cantidad de personas: ";
-    cin >> ax1;
-    r[cant].setCantidadPersonas(ax1);
-    cout << "  Ingrese la fecha de ingreso: ";
-    cin >> ax2;
-    r[cant].setFechaIngreso(ax2);
-    cout << "  Ingrese la fecha de salida: ";
-    cin >> ax2;
-    r[cant].setFechaSalida(ax2);
-    cout << r[cant].getCodigo() << endl;
-    cout << r[cant].getCantidadPErsonas() << endl;
-    cout << r[cant].getFechaIngreso() << endl;
-    cout << r[cant].getFechaSalida() << endl;
-    cant++;
-}
-static void verReservaciones(Reservacion r[50]){
-    string aux;
-    cout << "Ver Reservaciones" << endl;
-    cin >> aux;
-}
-static void buscarReservacion(Reservacion r[50], string busqueda){
-    string aux;
-    cout << "Buscar Reservaciones" << endl;
-    cin >> aux;
+#include "MetodosReservacion.cpp"
+int main() {
+    int cant = 0, op = 0;
+    Reservacion reservaciones[50];
+    while(op != 4) {
+        cout << "--Administracion de reservaciones--" << endl;
+        cout << "  Seleccione una opcion:" << endl;
+        cout << "  (1)Añadir reservacion" << endl;
+        cout << "  (2)Ver reservaciones" << endl;
+        cout << "  (3)Buscar reservaciones" << endl;
+        cout << "  (4)Volver" << endl;
+        cin >> op;
+        switch(op) {
+            case 1:
+                system("cls");
+                agregarReservacion(reservaciones,cant);
+                break;
+            case 2:
+                system("cls");
+                verReservaciones(reservaciones, cant);
+                break;
+            case 3:
+                system("cls");
+                buscarReservacion(reservaciones, "");
+                break;
+            case 4:
+                break;
+            default:
+                cout << "Opcion incorrecta: Ingrese una valida o 4 para volver" << endl;
+                break;
+        }
+    }
+    return 0;
 }
---
+++ MetodosReservacion.cpp
@@ -0,0 +1,58 @@
+//
+// Created by G-t-T on 1/07/2020.
+//
+#include <iostream>
+#include "Reservacion/Reservacion.h"
+#include "TipoHabitacion/TipoHabitacion.h"
+#include "Habitacion/Habitacion.h"
+
+static void agregarReservacion(Reservacion r[50], int& cant){
+    /*
+    Habitacion habitacion;
+    int codigo;
+    int cantidadPersonas;
+    time_t fechaIngreso;
+    time_t fechaSalida;
+     */
+    TipoHabitacion tp("individual", "una persona", 50);
+    int ax1;
+    time_t ax2;
+    cout << "->Agregar Reservaciones" << endl;
+    cout << "  Ingrese el codigo de reservacion: ";
+    cin >> ax1;
+    r[cant].setCodigo(ax1);
+    cout << "  Ingrese el numero de habitacion: ";
+    cin >> ax1;
+    Habitacion hab(ax1,1,tp);
+    r[cant].setHabitacion(hab);
+    cout << "  Ingrese la cantidad de personas: ";
+    cin >> ax1;
+    r[cant].setCantidadPersonas(ax1);
+    cout << "  Ingrese la fecha de ingreso: ";
+    cin >> ax2;
+    r[cant].setFechaIngreso(ax2);
+    cout << "  Ingrese la fecha de salida: ";
+    cin >> ax2;
+    r[cant].setFechaSalida(ax2);
+    cout << r[cant].getCodigo() << endl;
+    cout << r[cant].getCantidadPErsonas() << endl;
+    cout << r[cant].getFechaIngreso() << endl;
+    cout << r[cant].getFechaSalida() << endl;
+    cant++;
+}
+static void verReservaciones(Reservacion r[50], int cant){
+    for (int i = 0; i < cant; i++){
+        cout << "-------------------------" << endl;
+        cout << "Codigo: " << r[i].getCodigo() << endl;
+        cout << "Nº habitacion: " << r[i].getHabitacion().getNumHabitacion() << endl;
+        cout << "Cantidad de personas: " << r[i].getCantidadPErsonas() << endl;
+        cout << "Fecha de ingreso: " << r[i].getFechaIngreso() << endl;
+        cout << "Fecha de salida: " << r[i].getFechaSalida() << endl;
+        cout << "-------------------------" << endl;
+    }
+}
+static void buscarReservacion(Reservacion r[50], string busqueda){
+    string aux;
+    cout << "Buscar Reservaciones" << endl;
+    cin >> aux;
+}
