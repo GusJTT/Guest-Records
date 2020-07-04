f4ec28e3b0983e765e44f62b8435915741ffeced
Christian David Sullca Puma <csullcap@unsa.edu.pe>
2020-07-03 21:21:01 -0500

    Sistema inclusion con Base de Datos

---
+++ cmake-build-debug/CMakeFiles/PS.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Usuario/CLionProjects/Guest-Record/Conexion/Conexion.cpp" "C:/Users/Usuario/CLionProjects/Guest-Record/cmake-build-debug/CMakeFiles/PS.dir/Conexion/Conexion.cpp.obj"
+  "C:/Users/Usuario/CLionProjects/Guest-Record/Conexion/Conexion2.cpp" "C:/Users/Usuario/CLionProjects/Guest-Record/cmake-build-debug/CMakeFiles/PS.dir/Conexion/Conexion2.cpp.obj"
+  "C:/Users/Usuario/CLionProjects/Guest-Record/Funciones/funciones.cpp" "C:/Users/Usuario/CLionProjects/Guest-Record/cmake-build-debug/CMakeFiles/PS.dir/Funciones/funciones.cpp.obj"
+  "C:/Users/Usuario/CLionProjects/Guest-Record/main.cpp" "C:/Users/Usuario/CLionProjects/Guest-Record/cmake-build-debug/CMakeFiles/PS.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/MySQL/Connector C++ 8.0/include"
+  "C:/Program Files/MySQL/MySQL Server 8.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../ejecutable/PS.exe"
+  "../ejecutable/PS.exe.manifest"
+  "../ejecutable/PS.pdb"
+  "CMakeFiles/PS.dir/Conexion/Conexion.cpp.obj"
+  "CMakeFiles/PS.dir/Conexion/Conexion2.cpp.obj"
+  "CMakeFiles/PS.dir/Funciones/funciones.cpp.obj"
+  "CMakeFiles/PS.dir/main.cpp.obj"
+  "libPS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
